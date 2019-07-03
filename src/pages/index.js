9aff2fffa83396e260c058ead8e0a788809fe178
goodminkyo <minkyoleewsu@gmail.com>
2019-07-04 08:36:01 +0900

    MozziCheek| complete a skeleton code

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+/* pages/index.js
+   pages폴더 내에서 routing을 관리해주는 폴더입니다. 
+
+*/
+export { default as Home } from './Home'
+export { default as Aboutus } from './Aboutus'
+export { default as Login } from './Login'
