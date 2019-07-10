2ce736f3e461e3a59fa7d7470bb3242003032721
dmlimgo home <dongmyeong.lim@gmail.com>
2019-07-11 01:51:27 +0900

    [DM] Content example

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import CardView from './CardView'
+
 import '../assets/css/Content.css'
 
 export default class Content extends React.Component {
     render() {
         return (
             <div className="content-wrapper">
-                body
+                <CardView />
+                <CardView />
             </div>
         )
     }
