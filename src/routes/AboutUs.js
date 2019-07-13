3fd81cacf868e8ff0ee5ac47119d9f1ea332d5a5
dmlimgo home <dongmyeong.lim@gmail.com>
2019-07-13 23:31:01 +0900

    [dm] Aboutus | 로고이미지 파일 추가 및 경로 설정

--- src/routes/AboutUs.js
+++ src/routes/AboutUs.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../assets/css/AboutUs.css'
+import logo from '../assets/img/logo3.png'
 
 export default class AboutUs extends React.Component {
     render() {
         return (
             <div class="body-wrapper">
                 <div class="introduce-init">
-                    <img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/6d9e50ec-d45d-4349-a2db-959783be3917/logo3.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIAT73L2G45OTLMDOXF%2F20190711%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20190711T163907Z&X-Amz-Expires=86400&X-Amz-Security-Token=AgoJb3JpZ2luX2VjEE0aCXVzLXdlc3QtMiJHMEUCIEWfCif%2FPsFFITmI0P0%2Faj0yl4L%2BhjOg9BqddTfEo9A6AiEA%2BcyUywXBIi1TU7DLfzjKVkL6JvHOc9w44yV9YOXoyy0q4wMIpv%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARAAGgwyNzQ1NjcxNDkzNzAiDJi98mULBLNeu6ooFyq3A4Zj1b5IkKkf6mLiAWhNL6R0Zwc8ikR1MtJ1Soegbxf1AQ5%2B7mGc8Q1TGjQuh18cODKdwzH31ORbh%2FGrFHjhvVPDYX245NlLUXZZAfo8CqdU7WcKpEDBYvewkhYq50ySd6RDzuhFjxzlrB7OwlViAHhTqlJme87NHo9t67vGpUGChouF8yFw4P3UsDF32I39bBsqwuMqWe%2F7XYIITyLNkM0Gwr3M2rDadfT3gLl0rl2LfOJpUDuE2kjIJP54WQmZaacDRUikrbj4pTYzlK%2BONsG84Kq2qQcJ2tZUUxfU309WBu31lrRThlivORpp%2F%2FpGiEdeCH1Gk2eEm4p7MyXZKxUSbjk7hRbKTFV2fHiJE9jp3jMArQs08f90noAk3AXPBy%2BIbgh5b5IwtPLzOPqwOaNBDFHxVYZa%2B2l96Ey8145hULlnnXOzut3OUow%2FPx9Mz0SpmGsbYLCVb0GJ5vB4coJ1jFo%2B2o3OHqBE64kHDdIKT%2FD76Pte0JLpD4zGddnNfYLP4nsTw7xinAAgTGZAqfgr5OYQE1CWJmYvgNMHdEuPI7c9QXG9kzGGL4VJc%2F3ER6HA0nNnYMUw9dSc6QU6tAEoKjva4hSU8UA37DKt4XgAOaZiePN2%2F3BiklOGChY3qodrIruk%2FdVturEXW5ZAJav1DTVFrcPei%2FvuQORhce%2Fval8GrGR6jaH5uOAoPW3b7ihivEqAWYcUQZtVAZn%2BgbR3hOkuOf5W5fVif%2B0zMIrHcpIPXAjFA0RIFvUZvtlzpMUuwRdULnr%2FzvDKgcZ%2BnL%2Buq7AORK0C%2B%2B1Y%2B5A%2B%2BWfG9uAwE82JpntrHtSolYFQ1TDw8%2Fo%3D&X-Amz-Signature=af1d14841536be958dde9affe71e5a090650cdbcd4c907343ba72ce1b01f84da&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22logo3.png%22" class="logo"/>
+                    <img src={logo} class="logo"/>
                     <div class="content">
                         <h1>Creative & Passionate</h1>
                         <h1>Web Developers</h1>
