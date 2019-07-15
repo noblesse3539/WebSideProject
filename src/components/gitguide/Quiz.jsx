27dd5b49819d08f20c1e1465adb8155f95bc2a82
yooco0618 <yooco0618@gmail.com>
2019-07-15 18:13:18 +0900

    yooco| Sample App 제거 및 Quiz 컴포넌트 생성

--- src/components/gitguide/Quiz.jsx
+++ src/components/gitguide/Quiz.jsx
@@ -1,46 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Question from './Question';
-import QuestionCount from './QuestionCount';
-import AnswerOption from './AnswerOption';
 
+import CourseName from './CourseName';
 
-function Quiz(props) {
-    function renderAnswerOptions(key) {
+class Quiz extends React.Component {
+    render() {
         return (
-            <AnswerOption
-                key={key.content}
-                answerContent={key.content}
-                answerType={key.type}
-                answer={props.answer}
-                questionId={props.questionId}
-                onAnswerSelected={props.onAnswerSelected}
-            />
-        );
+            <div className="quiz">
+                <CourseName name="Basic" />
+            </div>
+        )
     }
-    
-    return (
-        <div className="quiz">
-            <QuestionCount
-                counter={props.questionId}
-                total={props.questionTotal}
-            />
-            <Question content={props.question} />
-            <ul className="answerOptions">
-                {props.answerOptions.map(renderAnswerOptions)}
-            </ul>
-        </div>
-    );
 }
-
-Quiz.propTypes = {
-    answer: PropTypes.string,
-    answerOptions: PropTypes.array,
-    counter: PropTypes.number,
-    question: PropTypes.string,
-    questionId: PropTypes.number,
-    questionTotal: PropTypes.number,
-    onAnswerSelected: PropTypes.func
-};
 
 export default Quiz;
---
+++ src/components/gitguide/api/Questions.js
@@ -0,0 +1,23 @@
+let questions = [
+    Basic = [
+        {
+            topic: "새로운 저장소 만들기",
+            question: "프로젝트 폴더를 생성하여 Git으로 관리하려고 합니다. 새로 생성한 폴더를 Git 저장소로 초기화하는 명령어를 입력하세요.",
+            answer: "git init",
+            hint: "git ____",
+            terminalResult: ""
+        },
+        {
+            topic: "다른 저장소 받아오기",
+            question: "친구 창오와 함께 프로젝트를 진행하려고 합니다. 창오의 스켈레톤 코드가 담긴 원격 저장소를 받아오려고 합니다. 다음과 같은 주소를 가진 저장소를 나의 컴퓨터 환경에 받아오기 위한 명령어를 입력하세요.",
+            answer: "git clone https://github.com/yooco0618/GitGuide.git",
+            hint: "___ _____ https://github.com/yooco0618/GitGuide.git",
+            terminalResult: ""
+        },
+    ],
+    Advanced = [
+
+    ]
+]
+
+export default questions;
