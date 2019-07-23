7ff35569e619a4df2b93872cf2bfb3909b623621
dmlimgo <dongmyeong.lim@gmail.com>
2019-07-24 03:41:55 +0900

    [DM] Home 레이아웃 구성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import TopNav from 'components/navbar/TopNav'
+
+import './Home.scss'
 
 export default class Home extends React.Component {
     render() {
         return (
-            <div></div>
+            <div>
+                <TopNav/>
+                <div className="aboutus-wrapper">
+                    <div className="introduce lmk">
+                        minkyo's pic & short quotes
+                    </div>
+                    <div className="introduce yco">
+                        chango's pic & short quotes
+                    </div>
+                    <div className="introduce kbs">
+                        byeongsuk's pic & short quotes
+                    </div>
+                    <div className="introduce ldm">
+                        dongmyeong's pic & short quotes
+                    </div>
+                    <div className="introduce all">
+                        we are webengers
+                    </div>
+                </div>
+                <div className="course-button">
+                    <button className="button">COURSES</button>
+                </div>
+            </div>
         );
     }
 };
