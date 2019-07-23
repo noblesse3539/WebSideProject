b0ba2e69c8d8ae2539fa8a230c863f7f10da5b02
dmlimgo <dongmyeong.lim@gmail.com>
2019-07-24 04:11:55 +0900

    [DM] course / layout 50%

--- src/routes/Course.js
+++ src/routes/Course.js
@@ -9,11 +9,12 @@ export default class Course extends React.Component {
         return (
             <div>
                 <TopNav/>
-                <div className="aboutus-wrapper">
-                    course
-                </div>
-                <div className="course-button">
-                    <button className="button">COURSES</button>
+                <div className="course-wrapper">
+                    <div className="course-flex">
+                        <div className="course-box">
+
+                        </div>
+                    </div>
                 </div>
                 <Footer/>
             </div>
